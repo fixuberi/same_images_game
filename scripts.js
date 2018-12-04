5078c0028579f82ac30111809ecced9856676aec
fixuberi <comeclarity007@gmail.com>
2018-12-04 16:40:15 +0200

    implemented: timer,  showing result and reset-button when the game is completed

--- scripts.js
+++ scripts.js
@@ -13,7 +13,10 @@ let images = [
     'https://kde.link/test/9.png'
 ];
 initGame();
-var firstEl, secondEl;
+var firstEl, 
+    secondEl, 
+    timerId;
+var timerValue = 1000;
 
 function handleCardClick() {
     if (!firstEl) {
@@ -36,9 +39,10 @@ function checkCoincidence(firstEl, secondEl) {
         setTimeout(function() {
             args.map(el => {
                 el.classList.remove('selected');
-                el.classList.add('completed')
+                el.classList.add('completed');
+                checkGameProcess();
             })
-        }, 200)
+        }, 200);
     } else {
         setTimeout(function() {
             args.map(el => {
@@ -51,7 +55,10 @@ function checkCoincidence(firstEl, secondEl) {
 }
 
 function checkGameProcess() {
-    
+    let generalCardsCount = document.getElementsByClassName('card').length;
+    let completedCardsCount = document.getElementsByClassName('completed').length;
+
+    (completedCardsCount == generalCardsCount) && finishGame();
 }
 
 function resetSteps() {
@@ -64,58 +71,94 @@ async function initGame() {
     let size = await responseSize.json();
     let cardsCount = size.width * size.height;
     let gameBox = document.getElementById('game-box')
+    let gameScore = document.getElementById('game-score')
 
     gameBox.style.width = (96+2)*size.width + 'px';
     gameBox.innerHTML = makeCardsCollectionHtml(cardsCount);
     let cards = document.querySelectorAll('.card');
     cards.forEach(card => card.addEventListener('click', handleCardClick));
+
+    updateTimer(timerValue);
+    timerId = setInterval(() => updateTimer(--timerValue), 1000)
+
+    function updateTimer(timerValue) {
+        gameScore.innerHTML = `<div id="timer">${timerValue}</div>`;
+    }
+
+    function makeCardsCollectionHtml(cardsCount) {
+        let cards = fillCollection(cardsCount);
+        return shuffle(cards).join('');
+    
+        function fillCollection(cardsCount) {
+            let cardsCollection = [];
+            let i = 0;
+            while(cardsCollection.length < cardsCount) {
+                let tempCard = makeCard(images[i]);
+                cardsCollection.push(tempCard, tempCard);
+                if (i < images.length - 1) {
+                        i++;
+                        continue;
+                    } else {
+                       images = shuffle(images); 
+                        i = 0;
+                    }
+            }
+            return cardsCollection
+        }
+    
+        function makeCard(imgUrl) {
+            return `<div class="card" data-number="${getImageId(imgUrl)}">
+                        <div class="back-face"></div>
+                        ${makeImage(imgUrl)}
+                    </div>`;
+        
+            function makeImage(url) {
+                return `<img src="${url}" class="front-face">`
+            }
+    
+            function getImageId(url) {
+                return url.split('/').slice(-1)[0].split('.')[0]
+            }
+        }
+        
+        function shuffle(arr){
+            var j, temp;
+            for(var i = arr.length - 1; i > 0; i--){
+                j = Math.floor(Math.random()*(i + 1));
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+    }
 }
 
-function makeCardsCollectionHtml(cardsCount) {
-    let cards = fillCollection(cardsCount);
-    return shuffle(cards).join('');
+function finishGame() {
+    clearInterval(timerId);
+    showTotal();
 
-    function fillCollection(cardsCount) {
-        let cardsCollection = [];
-        let i = 0;
-        while(cardsCollection.length < cardsCount) {
-            let tempCard = makeCard(images[i]);
-            cardsCollection.push(tempCard, tempCard);
-            if (i < images.length - 1) {
-                    i++;
-                    continue;
-                } else {
-                   images = shuffle(images); 
-                    i = 0;
-                }
-        }
-        return cardsCollection
-    }
+    function showTotal() {
+        let totalScore = timerValue > 0 ? timerValue : 0;
+        let gameScore = document.getElementById('game-score');
 
-    function makeCard(imgUrl) {
-        return `<div class="card" data-number="${getImageId(imgUrl)}" onclick>
-                    <div class="back-face"></div>
-                    ${makeImage(imgUrl)}
-                </div>`;
-    
-        function makeImage(url) {
-            return `<img src="${url}" class="front-face">`
-        }
+        gameScore.innerHTML = totalScoreHtml(totalScore) + resetButtonHtml();
+        document.getElementById('reset-button')
+                .addEventListener('click', handleResetButtonClick);
 
-        function getImageId(url) {
-            return url.split('/').slice(-1)[0].split('.')[0]
+        function totalScoreHtml(totalScore) {
+            return `<div class="game-result">
+                        End! Your score is <b>${totalScore}</b>
+                    </div>`;
         }
-    }
-    
-    function shuffle(arr){
-        var j, temp;
-        for(var i = arr.length - 1; i > 0; i--){
-            j = Math.floor(Math.random()*(i + 1));
-            temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
+        function resetButtonHtml() {
+            return `<div id="reset-button">New game</div>`;
         }
-        return arr;
+        function handleResetButtonClick() {
+            location.reload();
+        }
     }
 }
+
+
 
